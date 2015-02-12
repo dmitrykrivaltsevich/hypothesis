37545cd59252f8b825bedbd20e489821e69c2cb3
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2015-02-12 08:48:20 +0300

    reduced an RAM consumption in 100 times by using hypothesis stream

--- src/main/java/com/conjuncte/hypothesis/App.java
+++ src/main/java/com/conjuncte/hypothesis/App.java
@@ -3,7 +3,8 @@ package com.conjuncte.hypothesis;
 import com.conjuncte.hypothesis.domain.*;
 
 import java.math.BigInteger;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Hello world!
@@ -11,217 +12,8 @@ import java.util.*;
 public class App {
 
     // todo: add initial hypothesises
-    private Queue<Hypothesis> hypothesises = new LinkedList<Hypothesis>() {{
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("1", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("1", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("3", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("1", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("7", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("1", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("9", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("1", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("3", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("3", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("7", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("3", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("9", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("3", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("7", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("7", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("9", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("7", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
-        add(new Hypothesis() {
-            @Override
-            public Pair<Register, Register> getFactors() {
-                return new Pair<Register, Register>() {
-                    @Override
-                    public Register getFirst() {
-                        return new FreeRadixRegister("9", 10);
-                    }
-
-                    @Override
-                    public Register getSecond() {
-                        return new FreeRadixRegister("9", 10);
-                    }
-                };
-            }
-
-            @Override
-            public Integer getCellOffsetToCheck() {
-                return 0;
-            }
-        });
+    private Queue<Stream<Hypothesis>> hypothesises = new LinkedList<Stream<Hypothesis>>() {{
+        add(new InitialHypothesisStream());
     }};
 
 
@@ -256,60 +48,42 @@ public class App {
     }
 
     private Pair<Register, Register> checkAllHypothesises(Register targetProduct) {
-        Hypothesis hypothesis;
-        while ((hypothesis = hypothesises.poll()) != null) {
-            Pair<Register, Register> factors = hypothesis.getFactors();
-            Register factorsFirst = factors.getFirst();
-            Register factorsSecond = factors.getSecond();
-            Integer cellOffsetToCheck = hypothesis.getCellOffsetToCheck();
+        Stream<Hypothesis> hypothesisStream;
+        while ((hypothesisStream = hypothesises.poll()) != null) {
+            while (hypothesisStream.hasNext()) {
+                Hypothesis hypothesis = hypothesisStream.next();
+
+                Pair<Register, Register> factors = hypothesis.getFactors();
+                Register factorsFirst = factors.getFirst();
+                Register factorsSecond = factors.getSecond();
+                Integer cellOffsetToCheck = hypothesis.getCellOffsetToCheck();
 
 //            System.out.println(String.format("Check hypothesis: %s, %s, %s",
 //                    factorsFirst,
 //                    factorsSecond,
 //                    cellOffsetToCheck));
 
-            Register partialProduct = factorsFirst.partialProduct(factorsSecond, cellOffsetToCheck);
-            // todo: seems we don't need this extra-check
-            if (!partialProduct.hasCell(cellOffsetToCheck)) {
-                continue;
-            }
+                Register partialProduct = factorsFirst.partialProduct(factorsSecond, cellOffsetToCheck);
+                // todo: seems we don't need this extra-check
+                if (!partialProduct.hasCell(cellOffsetToCheck)) {
+                    continue;
+                }
 
-            if (targetProduct.getCell(cellOffsetToCheck).equals(partialProduct.getCell(cellOffsetToCheck))) {
-                if (targetProduct.getCapacity().equals(partialProduct.getCapacity())
-                        && targetProduct.equals(partialProduct)) {
-                    return factors;
-                } else if (factorsFirst.getCapacity() + factorsSecond.getCapacity() < targetProduct.getCapacity()) {
-                    Collection<Hypothesis> derivedHypothesises = createHypothesises(
-                            factors,
-                            cellOffsetToCheck + 1);
-                    hypothesises.addAll(derivedHypothesises);
+                if (targetProduct.getCell(cellOffsetToCheck).equals(partialProduct.getCell(cellOffsetToCheck))) {
+                    if (targetProduct.getCapacity().equals(partialProduct.getCapacity())
+                            && targetProduct.equals(partialProduct)) {
+                        return factors;
+                    } else if (factorsFirst.getCapacity() + factorsSecond.getCapacity() < targetProduct.getCapacity()) {
+                        Stream<Hypothesis> derivedHypothesisesStream = new CartesianProductStream(
+                                factors,
+                                cellOffsetToCheck + 1);
+                        hypothesises.add(derivedHypothesisesStream);
+                    }
                 }
             }
         }
 
         // todo: add guava optional
         return null;
-    }
-
-    private Collection<Hypothesis> createHypothesises(final Pair<Register, Register> factors, final int cellOffsetToCheck) {
-//        System.out.println(String.format("Creating derived hypothesis for: %s, %s, %s",
-//                factors.getFirst(),
-//                factors.getSecond(),
-//                cellOffsetToCheck - 1));
-
-        // todo: dirty implementation - knows about radix = 10
-        // todo: add proper collection initialization
-        List<Hypothesis> newHypothesises = new ArrayList<Hypothesis>();
-        for (final Cell cellFirst : factors.getFirst().getPossibleCellValues()) {
-            for (final Cell cellSecond : factors.getSecond().getPossibleCellValues()) {
-                newHypothesises.add(new HypothesisImpl(
-                        new FreeRadixRegister(cellFirst.getValue() + factors.getFirst().toString(), 10),
-                        new FreeRadixRegister(cellSecond.getValue() + factors.getSecond().toString(), 10),
-                        cellOffsetToCheck
-                ));
-            }
-        }
-
-        return newHypothesises;
     }
 }
