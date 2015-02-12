a339299f8f2436bcd296577b44ffbe856a21298d
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2015-02-12 08:42:22 +0300

    added cartesian product stream of hypothesis

---
+++ src/main/java/com/conjuncte/hypothesis/domain/CartesianProductStream.java
@@ -0,0 +1,53 @@
+package com.conjuncte.hypothesis.domain;
+
+import com.google.common.collect.Iterators;
+
+import java.util.Collection;
+
+public class CartesianProductStream
+        implements Stream<Hypothesis> {
+
+    private final Pair<Register, Register> registers;
+    private final int cellOffset;
+
+    private int firstFactorOffset = 0;
+    private int secondFactorOffset = 0;
+
+    public CartesianProductStream(Pair<Register, Register> registers, int cellOffset) {
+        assert registers != null;
+        assert registers.getFirst() != null;
+        assert registers.getSecond() != null;
+        assert cellOffset >= 0;
+
+        this.registers = registers;
+        this.cellOffset = cellOffset;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return firstFactorOffset < registers.getFirst().getPossibleCellValues().size()
+                && secondFactorOffset < registers.getSecond().getPossibleCellValues().size();
+    }
+
+    @Override
+    public Hypothesis next() {
+        Collection<Cell> possibleCellValuesFirstRegister = registers.getFirst().getPossibleCellValues();
+        Collection<Cell> possibleCellValuesSecondRegister = registers.getSecond().getPossibleCellValues();
+
+        HypothesisImpl hypothesis = new HypothesisImpl(
+                new FreeRadixRegister(
+                        Iterators.get(possibleCellValuesFirstRegister.iterator(), firstFactorOffset).getValue()
+                                + registers.getFirst().toString(), 10),
+                new FreeRadixRegister(
+                        Iterators.get(possibleCellValuesSecondRegister.iterator(), secondFactorOffset).getValue()
+                                + registers.getSecond().toString(), 10),
+                cellOffset);
+
+        if (++firstFactorOffset == possibleCellValuesFirstRegister.size()) {
+            secondFactorOffset += 1;
+            firstFactorOffset = 0;
+        }
+
+        return hypothesis;
+    }
+}
---
+++ src/test/java/com/conjuncte/hypothesis/domain/CartesianProductStreamUnitTest.java
@@ -0,0 +1,42 @@
+package com.conjuncte.hypothesis.domain;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+public class CartesianProductStreamUnitTest {
+
+    @Test
+    public void testStreamValues() throws Exception {
+        // Given
+        Stream<Hypothesis> stream = new CartesianProductStream(new Pair<Register, Register>() {
+            @Override
+            public Register getFirst() {
+                return new FreeRadixRegister("1", 10);
+            }
+
+            @Override
+            public Register getSecond() {
+                return new FreeRadixRegister("1", 10);
+            }
+        }, 1);
+
+        // When
+        Collection<Hypothesis> hypothesises = new ArrayList<Hypothesis>();
+        while (stream.hasNext()) {
+            hypothesises.add(stream.next());
+        }
+
+        Collection<Register> registers = new ArrayList<Register>();
+        for (Hypothesis hypothesis : hypothesises) {
+            registers.add(hypothesis.getFactors().getFirst());
+            registers.add(hypothesis.getFactors().getSecond());
+        }
+
+        // Then
+        assertEquals(162, registers.size());
+    }
+}
